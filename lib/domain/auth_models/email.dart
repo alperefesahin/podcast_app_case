efd86c1d25ddbd92624ee4a9cd503d2393880c4d
sahinefe <67283777+alper-efe-sahin@users.noreply.github.com>
2022-05-11 12:24:20 +0300

    chore: domain layer/auth models

---
+++ lib/domain/auth_models/email.dart
@@ -0,0 +1,31 @@
+import 'package:formz/formz.dart';
+
+enum EmailValidationError { invalid }
+
+class Email extends FormzInput<String, EmailValidationError> {
+  const Email.pure() : super.pure('');
+  const Email.dirty([String value = '']) : super.dirty(value);
+
+  static final RegExp _emailRegExp = RegExp(
+    r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$',
+  );
+
+  @override
+  EmailValidationError? validator(String value) {
+    if (value.isEmpty) {
+      return null;
+    }
+    return _emailRegExp.hasMatch(value) && value.length < 30 ? null : EmailValidationError.invalid;
+  }
+}
+
+extension Explanation on EmailValidationError {
+  String? get name {
+    switch (this) {
+      case EmailValidationError.invalid:
+        return "Invalid email";
+      default:
+        return null;
+    }
+  }
+}
---
+++ lib/domain/auth_models/password.dart
@@ -0,0 +1,29 @@
+import 'package:formz/formz.dart';
+
+enum PasswordValidationError { invalid, empty }
+
+class Password extends FormzInput<String, PasswordValidationError> {
+  const Password.pure() : super.pure('');
+  const Password.dirty([String value = '']) : super.dirty(value);
+
+  static final _passwordRegExp = RegExp(r'^[A-Za-z\d@$!%*?&]{8,}$');
+
+  @override
+  PasswordValidationError? validator(String value) {
+    if (value.isEmpty) {
+      return PasswordValidationError.empty;
+    }
+    return _passwordRegExp.hasMatch(value) && value.length < 10 ? null : PasswordValidationError.invalid;
+  }
+}
+
+extension Explanation on PasswordValidationError {
+  String? get name {
+    switch (this) {
+      case PasswordValidationError.invalid:
+        return "Invalid password";
+      default:
+        return null;
+    }
+  }
+}
