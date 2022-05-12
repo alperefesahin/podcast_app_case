48120702dd5d6e1a31ce8c64bc1e928ae70f1cf8
sahinefe <67283777+alper-efe-sahin@users.noreply.github.com>
2022-05-12 18:36:56 +0300

    refactor: refactor home page body

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:crop_experience_agency_case/infrastructure/api/api.dart';
 import 'package:crop_experience_agency_case/presentation/pages/home/constants/colors.dart';
 import 'package:crop_experience_agency_case/presentation/pages/home/constants/texts.dart';
 import 'package:crop_experience_agency_case/presentation/pages/home/widgets/green_straight_line.dart';
+import 'package:crop_experience_agency_case/presentation/pages/home/widgets/home_page_body.dart';
 import 'package:crop_experience_agency_case/presentation/pages/home/widgets/notification_icon.dart';
 import 'package:crop_experience_agency_case/presentation/pages/home/widgets/screen_tab.dart';
 import 'package:crop_experience_agency_case/presentation/pages/home/widgets/search_bar.dart';
@@ -25,29 +26,7 @@ class HomePage extends StatelessWidget {
               if (!snapshot.hasData) {
                 return const Center(child: CircularProgressIndicator());
               } else {
-                return Column(
-                  children: [
-                    Row(
-                      children: const [
-                        Expanded(child: SearchBar()),
-                        NotificationIcon(),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Column(
-                          children: const [
-                            ScreenTab(text: forYou, fontWeight: FontWeight.w500, textColor: whiteColor),
-                            GreenStraightLine(),
-                          ],
-                        ),
-                        const ScreenTab(text: podcast, fontWeight: FontWeight.w400, textColor: screenTabTextsColor),
-                        const ScreenTab(text: radio, fontWeight: FontWeight.w400, textColor: screenTabTextsColor),
-                        const ScreenTab(text: audiobook, fontWeight: FontWeight.w400, textColor: screenTabTextsColor),
-                      ],
-                    )
-                  ],
-                );
+                return HomePageBody();
               }
             }
             return const Center(child: CircularProgressIndicator());
---
+++ lib/presentation/pages/home/widgets/home_page_body.dart
@@ -0,0 +1,38 @@
+import 'package:crop_experience_agency_case/presentation/pages/home/constants/colors.dart';
+import 'package:crop_experience_agency_case/presentation/pages/home/constants/texts.dart';
+import 'package:crop_experience_agency_case/presentation/pages/home/widgets/green_straight_line.dart';
+import 'package:crop_experience_agency_case/presentation/pages/home/widgets/notification_icon.dart';
+import 'package:crop_experience_agency_case/presentation/pages/home/widgets/screen_tab.dart';
+import 'package:crop_experience_agency_case/presentation/pages/home/widgets/search_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePageBody extends StatelessWidget {
+  const HomePageBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: const [
+            Expanded(child: SearchBar()),
+            NotificationIcon(),
+          ],
+        ),
+        Row(
+          children: [
+            Column(
+              children: const [
+                ScreenTab(text: forYou, fontWeight: FontWeight.w500, textColor: whiteColor),
+                GreenStraightLine(),
+              ],
+            ),
+            const ScreenTab(text: podcast, fontWeight: FontWeight.w400, textColor: screenTabTextsColor),
+            const ScreenTab(text: radio, fontWeight: FontWeight.w400, textColor: screenTabTextsColor),
+            const ScreenTab(text: audiobook, fontWeight: FontWeight.w400, textColor: screenTabTextsColor),
+          ],
+        )
+      ],
+    );
+  }
+}
