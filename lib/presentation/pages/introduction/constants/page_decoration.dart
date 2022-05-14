6a8b448c0ff41299dadff33f68ffef7fc82f9f8d
sahinefe <67283777+alper-efe-sahin@users.noreply.github.com>
2022-05-14 15:57:15 +0300

    fix: introduction img padding

--- lib/presentation/pages/introduction/constants/page_decoration.dart
+++ lib/presentation/pages/introduction/constants/page_decoration.dart
@@ -6,5 +6,6 @@ const pageDecoration = PageDecoration(
   imageAlignment: Alignment.topCenter,
   titlePadding: EdgeInsets.only(top: 80, bottom: 6, left: 12, right: 12),
   bodyPadding: EdgeInsets.only(bottom: 10, left: 12, right: 12),
+  imagePadding: EdgeInsets.only(left: 25, right: 25),
   bodyFlex: 0,
 );
